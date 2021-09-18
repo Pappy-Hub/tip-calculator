8d6d4d215dcf671e70f1d2479c49f9a54ff7b817
Emeka Ewelike <emekaewelike@gmail.com>
2021-09-18 13:38:50 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import react, {useState} from 'react';
 import './App.css';
+import {BiDollar} from 'react-icons/bi'
 
 function App() {
+  const [value, setValue] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <p className='header'>SPLITTER</p>
+      <section className='section'>
+        <div className='divone'>
+          Bill <br/>
+          <div className='divoneinner'>
+            <BiDollar className='dollar'/>
+            <input type='text' className='billinput' value={value} onChange={(e)=> setValue(e.target.value)}/>
+          </div>
+          <div className="percentage">
+            <h5>Select Tip %</h5>
+            <div className='custom'>
+              <section className='customone'>
+                <button>5%</button>
+                <button>10%</button>
+                <button>15%</button>
+              </section>
+              <section className='customtwo'>
+                <button>25%</button>
+                <button>50%</button>
+                <div className='custominputdiv'><input placeholder='CUSTOM'/></div>
+              </section>
+            </div>
+          </div>
+        </div>
+        <div className='divtwo'>
+          two
+        </div>
+      </section>
+    </section>
   );
 }
 
